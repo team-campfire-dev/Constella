9de3c1a1e3588ee495d1b6487187996c0a16b044
minjae <zeratulspc@gmail.com>
2026-02-03 15:19:22 +0900

    agent용 로그인 계정 관련 수정

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -41,7 +41,7 @@ export default async function RootLayout(props: {
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
       >
